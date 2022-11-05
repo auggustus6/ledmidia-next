634cbc2f9350acd0c8708e529f40bc62c1d4c761
guusfelix2015 <gusfelix2015@gmail.com>
2022-11-05 10:56:27 -0300

    Fix: text

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -8,7 +8,7 @@ const Contact = () => {
     <S.Contact id="contact" ref={elementRef}>
       <S.ContainerContact>
         <S.ContentContact>
-          <h2>Peça seu orgamento agora!</h2>
+          <h2>Peça seu orçamento agora!</h2>
           <p>
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit. Exercitation
