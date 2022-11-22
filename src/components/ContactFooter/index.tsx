4e50b96eaeecc6fba5783fbb776aeff96f0aee02
Carlos Augusto <auggustus6@gmail.com>
2022-11-22 20:03:17 -0300

    feat: add mail

--- src/components/ContactFooter/index.tsx
+++ src/components/ContactFooter/index.tsx
@@ -5,15 +5,21 @@ const ContactFooter = () => {
     <S.FooterContact>
       <h3>Contato</h3>
       <p>+55 17 99262-3038</p>
-      <p>Instagram:</p>
+      <p style={{ marginBottom: -5 }}>Instagram:</p>
       <p>
         <a
-          style={{ color: "#d316aa" }}
+          style={{ color: "#00a6de" }}
           href="https://instagram.com/ledmidiarp"
           target="_blank"
           rel="noreferrer"
         >
           @ledmidiarp
+        </a>
+      </p>
+      <p style={{ marginBottom: -5 }}>Email:</p>
+      <p>
+        <a style={{ color: "#00a6de" }} href="mailto:contato@ledmidiarp.com.br">
+          contato@ledmidiarp.com.br
         </a>
       </p>
     </S.FooterContact>
