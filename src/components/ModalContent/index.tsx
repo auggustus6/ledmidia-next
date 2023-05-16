7d0c2e82efe0dd5975d75ae4618de5b26649d945
ChrisCoy <christopherleegs@outlook.com>
2023-05-16 11:19:56 -0300

    fix: fix maps

--- src/components/ModalContent/index.tsx
+++ src/components/ModalContent/index.tsx
@@ -63,7 +63,7 @@ export default function ModalContent({
           // style={{ backgroundImage: `url(/img/guarujaHover.png)` }}
           >
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d783.9725595030973!2d-49.38576947027319!3d-20.822700319409243!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94bdb3d09556c195%3A0x7f88c06355b215a2!2sPosto%20Monte%20Carlo%20Guaruj%C3%A1!5e0!3m2!1spt-BR!2sbr!4v1673996838521!5m2!1spt-BR!2sbr"
+              src={map}
               allowFullScreen={false}
               referrerPolicy="no-referrer-when-downgrade"
             />
